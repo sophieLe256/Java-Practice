79a81e16b4e805066da1bf2262cdbe8faa736ddd
Sophie Le <dinokuro256@gmail.com>
2022-10-12 13:43:23 -0400

    Update 10/12

---
+++ src/academy/programming/GroceryList.java
@@ -0,0 +1,49 @@
+package academy.programming;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
+public class GroceryList {
+    public static void main(String[] args) {
+
+    }
+//    private int[] myNumbers = new int[50];
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addGroceryItem(String item) {
+      groceryList.add(item);
+
+    }
+
+    public void printGroceryList(){
+        System.out.println("You have " + groceryList.size() + " items in your list ");
+        for(int i = 0; i < groceryList.size() ; i++){
+            //i+1 cuz we want to start to count from 1
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(int position, String newItem){
+        groceryList.add(position, newItem);
+        //add 1 to position cuz index always start at 0
+        System.out.println("Grocery item " + (position+1) + " has been modified.");
+    }
+
+    public void removeGroceryItem(int position){
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+    }
+
+    public String findItem(String searchItem){
+        //returns true if this list contains the specified element
+        //More formally, return true if and only this list contains at least one element
+//        boolean exits = groceryList.contains(searchItem);
+
+        int position = groceryList.indexOf(searchItem); // search the arrayList and find the items
+        if(position >= 0){
+            return groceryList.get(position);
+        }
+
+        return null;
+    }
+}
--- src/challenge/coding/ReverseArray.java
+++ src/challenge/coding/ReverseArray.java
@@ -32,5 +32,14 @@ public class ReverseArray {
         }
     }
 
+    // shorter way to reverse the array
+
+//    for(int i = 0; i < validData.length / 2; i++)
+//    {
+//        int temp = validData[i];
+//        validData[i] = validData[validData.length - i - 1];
+//        validData[validData.length - i - 1] = temp;
+//    }
+
 
 }
