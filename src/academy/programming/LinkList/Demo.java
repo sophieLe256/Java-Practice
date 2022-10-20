036a8b48b989a5b2c0f103c74ae7627944d049a8
Sophie Le <dinokuro256@gmail.com>
2022-10-20 15:39:43 -0400

    Update 10/20

---
+++ src/academy/programming/LinkList/Demo.java
@@ -0,0 +1,70 @@
+package academy.programming.LinkList;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class Demo {
+    public static void main(String[] args) {
+        LinkedList<String> placesToVisit = new LinkedList<String>();
+//        placesToVisit.add("Sydney");
+//        placesToVisit.add("Melbourne");
+//        placesToVisit.add("Brisbane");
+//        placesToVisit.add("Perth");
+//        placesToVisit.add("Canberra");
+//        placesToVisit.add("Adelaide");
+//        placesToVisit.add("Darwin");
+
+        addInOrder(placesToVisit, "Sydney");
+        addInOrder(placesToVisit, "Melbourne");
+        addInOrder(placesToVisit, "Brisbane");
+        addInOrder(placesToVisit, "Perth");
+        addInOrder(placesToVisit, "Canberra");
+        addInOrder(placesToVisit, "Adelaide");
+        addInOrder(placesToVisit, "Darwin");
+        printList(placesToVisit);
+
+//        addInOrder()
+
+
+
+
+        printList(placesToVisit);
+
+        placesToVisit.add(1, "Alice Springs");
+        printList(placesToVisit);
+
+        placesToVisit.remove(4);
+        printList(placesToVisit);
+    }
+
+    private static void printList(LinkedList<String> linkedList){
+        Iterator<String> i = linkedList.iterator();
+        while(i.hasNext()){
+            System.out.println("Now visiting " + i.next());
+        }
+        System.out.println("=========================");
+    }
+
+    private static boolean addInOrder(LinkedList<String> linkedList, String newCity){
+        ListIterator<String> stringListIterator = linkedList.listIterator();
+
+
+        while(stringListIterator.hasNext()){
+            int comparison = stringListIterator.next().compareTo(newCity);
+            if(comparison == 0){
+                //equal, do not add
+                System.out.println(newCity + " is already included as a destination ");
+                return  false;
+            } else if(comparison > 0){
+                //new City should appear before this one
+                //Brisbane -> Adelaide
+                stringListIterator.previous();
+                stringListIterator.add(newCity);
+                return  true;
+            } else if (comparison < 0){
+                //move on next city
+            }
+        }
+    }
+}
